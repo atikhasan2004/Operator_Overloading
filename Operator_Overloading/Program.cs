bdf79e85e2bf1ee54ce3ca583d88b7b8a9ecbd94
Atik Hasan <saatikhasan5021@gmail.com>
2026-02-04 15:24:13 +0600

    Add files via upload
    We will redefine new operators work for user defined classes and objects.
    1. public static method
    2.Must take parameters of the class type.

---
+++ Operator_Overloading/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Complex
+{
+    public int real;
+    public int img;
+    public Complex(int r , int i)
+    {
+        real = r;
+        img = i;
+    }
+    public static Complex operator + (Complex c1 , Complex c2)
+    {
+        return new Complex (c1.real + c2.real , c1.img + c2.img);
+    }
+    public void Display()
+    {
+        Console.WriteLine($"{real} + {img}i");
+    }
+}
+class Program
+{
+    static void Main(string [] args)
+    {
+        Complex a = new Complex (1 , 3);
+        Complex b  = new Complex (2 , 4);
+        Complex result = a + b;
+        result.Display();
+    }
+}
